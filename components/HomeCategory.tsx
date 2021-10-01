d3ab923b0dbc6e964fb34dc9804e039af437be7b
Abhishek Paul <paul.abhishek2000@gmail.com>
2021-10-02 04:21:49 +0530

    Home Screen Navigation

--- components/HomeCategory.tsx
+++ components/HomeCategory.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, Image, FlatList } from 'react-native';
+import { StyleSheet, Image, FlatList, Pressable } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
 import { Text } from '../components/Themed';
 
@@ -16,13 +17,21 @@ interface HomeCategoryProps {
 
 const HomeCategory = (props: HomeCategoryProps) => {
   const { category } = props
+  const navigation = useNavigation()
+
+  const moviePress = (movie) => {
+    navigation.navigate('MovieDetailScreen', {id: movie.id})
+  }
+
   return (
     <>
       <Text style={styles.title}> {category.title} </Text>
       <FlatList
         data={category.movies}
         renderItem={({ item }) => (
-          <Image style={styles.image} source={{uri: item.poster}} />
+          <Pressable onPress = {() => moviePress(item)}>
+            <Image style={styles.image} source={{uri: item.poster}} />
+          </Pressable>
         )}
         horizontal
         showsHorizontalScrollIndicator = {false}
